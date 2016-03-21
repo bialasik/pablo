e1373202ac310b5b637d48b6fffe59ea3d47581d
Dan Horan <dan.horan@me.com>
2016-03-21 20:58:41 +0000

    Updated Details.js Component

--- src/assets/scripts/containers/Detail.js
+++ src/assets/scripts/containers/Detail.js
@@ -1,7 +1,20 @@
 import React, { Component } from 'react';
 
 export default class Detail extends Component {
+  getSection(params, data) {
+    return params.split('/').map((param) => {
+      return data.directory.filter((current) => {
+        return current.name.toLowerCase() === param;
+      });
+    });
+  }
+
+  componentWillMount() {
+    this.section = this.getSection(this.props.routeParams.splat, window.routes);
+  }
+
   render() {
+    console.log(this);
     return (
       <div>
         { this.props.children }
