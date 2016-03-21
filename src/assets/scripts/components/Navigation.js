0002b33c7adf5f2ebfeabf9f8c49fe07c2423bc8
Dan Horan <dan.horan@me.com>
2016-03-21 20:41:57 +0000

    Added working navigation

--- src/assets/scripts/components/Navigation.js
+++ src/assets/scripts/components/Navigation.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router';
+
+const build = (current, path) => {
+  return (
+    <div>
+      <Link to={ `${path}/${current.name.toLowerCase()}` }>{ current.name }</Link>
+      { current.directory && current.directory.length ? current.directory.map((child) => build(child, `${path}/${current.name.toLowerCase()}`)) : null }
+    </div>
+  );
+};
 
 export default (props) => {
   return (
     <nav>
-      { props.data.directory.map((file) => <h1>{ file.name }</h1>) }
+      { props.data.directory.map((current) => build(current, '')) }
     </nav>
   );
 };
