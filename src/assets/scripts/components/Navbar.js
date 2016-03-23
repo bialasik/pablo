1816f865a825565f5edfe6f8d123379781d213f7
Dan Horan <dan.horan@me.com>
2016-03-23 19:15:33 +0000

    Added examples directory

--- src/assets/scripts/components/Navbar.js
+++ src/assets/scripts/components/Navbar.js
@@ -1,12 +1,22 @@
 import React from 'react';
 
 export default (props) => {
+  let toggle;
+
+  if (props.themes.length > 1) {
+    toggle = (
+      <nav>
+        { props.themes.map((theme) => {
+          return (<button onClick={ (event) => props.handler(theme.assets) }>{ theme.name }</button>);
+        }) }
+      </nav>
+    );
+  }
+
   return (
     <header>
       <h1>Pablo</h1>
-      <nav>
-        { props.themes.map((theme) => <button onClick={ (event) => props.handler(theme.assets) }>{ theme.name }</button>) }
-      </nav>
+      { toggle }
       <hr />
     </header>
   );
