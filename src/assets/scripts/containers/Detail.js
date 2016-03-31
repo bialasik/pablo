4349e421d7d42f4488fbb4031f2d1911effc5c5f
David Reid <davidreiduni@gmail.com>
2016-04-01 00:13:09 +0100

    Added in component switching based on url and iframe support

--- src/assets/scripts/containers/Detail.js
+++ src/assets/scripts/containers/Detail.js
@@ -1,29 +1,28 @@
 import React, { Component } from 'react';
 import ReactMarkdown from 'react-markdown';
 
+import Frame from './../components/Frame';
+
 const __INITIAL_STATE__ = window.__INITIAL_STATE__;
 
 export default class Detail extends Component {
   getSection(params, data) {
-    return params.split('/').map((param) => {
-      return data.directory.filter((current) => {
-        return current.name.toLowerCase() === param;
-      });
-    });
-  }
+    return params.split('/').reduce((prev, curr) => {
+      const currentDirectory = prev.directory.filter((directory) => {
+        return directory.name.toLowerCase() === curr.toLowerCase();
+      }).pop();
 
-  componentWillMount() {
-    this.section = this.getSection(this.props.routeParams.splat, __INITIAL_STATE__);
+      return Object.assign({}, currentDirectory);
+    }, data);
   }
 
   render() {
-    const example = '## Dropdown\n\nDropdown example';
+    this.section = this.getSection(this.props.routeParams.splat, __INITIAL_STATE__);
 
     return (
-      <div>
-        <ReactMarkdown source={ example } />
-        { this.props.children }
-      </div>
+      <Frame>
+        {this.section.files['01.default.html'] ? <ReactMarkdown source={ this.section.files['01.default.html'] } /> : <h1>Nothing to see mate.</h1>}
+      </Frame>
     );
   }
 };
